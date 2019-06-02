ebc61588662bc8ef1e3f8beb44383bb4ba480a9d
StoyanYanev <stoyanyanev98@gmail.com>
2019-06-02 15:10:57 +0300

    Update PC.sql

--- 08.DefiningRelations/PC.sql
+++ 08.DefiningRelations/PC.sql
@@ -15,23 +15,21 @@ CREATE TABLE Printer(
 	price DECIMAL(5, 2)
 );
 
-/*á)*/
+/*b)*/
 INSERT INTO Product
 VALUES('A', '1256', 'PC');
 
 INSERT INTO Printer
 VALUES(1, '1401', 150);
 
+/*v)*/
 ALTER TABLE Printer
 ADD type VARCHAR(6), color CHAR;
 
-/*â)*/
-ALTER TABLE Printer
-ADD type VARCHAR(6), color CHAR;
-/*ã)*/
+/*g)*/
 ALTER TABLE Printer
 DROP COLUMN price;
-/*ä)*/
+/*d)*/
 DROP TABLE Printer;
 
 DROP TABLE Product;
